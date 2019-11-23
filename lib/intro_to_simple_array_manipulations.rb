43125bea9d1f61e9b13e92db1c04c6ec421f6bd9
pledgedavis <pledgedavis14@gmail.com>
2019-11-23 17:01:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -47,8 +47,8 @@ return noodle.flatten
    array.delete(string)
 end  
  
- def using_delete_at(array,string)
-   array.delete(string)
+ def using_delete_at(array,index)
+   array.delete(index)
  end
  
  
